27ed6c8b1c1fbb6e106f8143b7565bc3d11ea8d1
João Guilherme <joaogfp07@gmail.com>
2024-01-15 08:13:47 -0300

    Update version-check.sh

--- version-check.sh
+++ version-check.sh
@@ -8,32 +8,35 @@ bail() { echo "FATAL: $1"; exit 1; }
 grep --version > /dev/null 2> /dev/null || bail "grep does not work"
 sed '' /dev/null || bail "sed does not work"
 sort /dev/null || bail "sort does not work"
+
 ver_check()
 {
-if ! type -p $2 &>/dev/null
-then
-echo "ERROR: Cannot find $2 ($1)"; return 1;
-fi
-v=$($2 --version 2>&1 | grep -E -o '[0-9]+\.[0-9\.]+[a-z]*' | head -n1)
-if printf '%s\n' $3 $v | sort --version-sort --check &>/dev/null
-then
-printf "OK: %-9s %-6s >= $3\n" "$1" "$v"; return 0;
-else
-printf "ERROR: %-9s is TOO OLD ($3 or later required)\n" "$1";
-return 1;
-fi
+  if ! type -p $2 &>/dev/null
+  then
+    echo "ERROR: Cannot find $2 ($1)"; return 1;
+  fi
+    v=$($2 --version 2>&1 | grep -E -o '[0-9]+\.[0-9\.]+[a-z]*' | head -n1)
+  if printf '%s\n' $3 $v | sort --version-sort --check &>/dev/null
+  then
+    printf "OK: %-9s %-6s >= $3\n" "$1" "$v"; return 0;
+  else
+    printf "ERROR: %-9s is TOO OLD ($3 or later required)\n" "$1";
+    return 1;
+  fi
 }
+
 ver_kernel()
 {
 kver=$(uname -r | grep -E -o '^[0-9\.]+')
 if printf '%s\n' $1 $kver | sort --version-sort --check &>/dev/null
 then
-printf "OK: Linux Kernel $kver >= $1\n"; return 0;
+  printf "OK: Linux Kernel $kver >= $1\n"; return 0;
 else
-printf "ERROR: Linux Kernel ($kver) is TOO OLD ($1 or later required)\n" "$kver";
-return 1;
+  printf "ERROR: Linux Kernel ($kver) is TOO OLD ($1 or later required)\n" "$kver";
+  return 1;
 fi
 }
+
 # Coreutils first because-sort needs Coreutils >= 7.0
 ver_check Coreutils sort 7.0 || bail "--version-sort unsupported"
 ver_check Bash bash 3.2
@@ -59,11 +62,13 @@ ver_kernel 4.14
 if mount | grep -q 'devpts on /dev/pts' && [ -e /dev/ptmx ]
 then echo "OK: Linux Kernel supports UNIX 98 PTY";
 else echo "ERROR: Linux Kernel does NOT support UNIX 98 PTY"; fi
+
 alias_check() {
 if $1 --version 2>&1 | grep -qi $2
 then printf "OK: %-4s is $2\n" "$1";
 else printf "ERROR: %-4s is NOT $2\n" "$1"; fi
 }
+
 echo "Aliases:"
 alias_check awk GNU
 alias_check yacc Bison
@@ -73,3 +78,4 @@ if printf "int main(){}" | g++ -x c++ -
 then echo "OK: g++ works";
 else echo "ERROR: g++ does NOT work"; fi
 rm -f a.out
+EOF
